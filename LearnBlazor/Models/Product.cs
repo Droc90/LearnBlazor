3137e0e676414eba7c3acc91667c453546d62b5f
Dan O <droconnell2000@yahoo.com>
2025-04-01 15:14:45 -0400

    Start new project

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+
+    }
+}
