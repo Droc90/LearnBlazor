ca207ba3a868665eacb6482c8d78e8e8b4f14f1a
Dan O <droconnell2000@yahoo.com>
2025-04-09 19:07:03 -0400

    End Section 7

---
+++ LearnBlazor/wwwroot/js/common.js
@@ -0,0 +1,38 @@
+﻿window.showToastr = function (type, message)
+{
+    if (type == "success") {
+        toastr.success(message);
+    }
+
+    if (type == "error") {
+        toastr.error(message);
+    }
+
+    if (type == "warning") {
+        toastr.warning(message);
+    }
+}
+window.showSwal = function (type, message) {
+    if (type == "success") {
+        Swal.fire({
+            title: "Good job!",
+            text: message,
+            icon: "success"
+        });
+    }
+    if (type == "error") {
+        Swal.fire({
+            title: "Task Failed!",
+            text: message,
+            icon: "error"
+        });
+    }
+    if (type == "warning") {
+        Swal.fire({
+            icon: 'warning',
+            title: message,
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+}
